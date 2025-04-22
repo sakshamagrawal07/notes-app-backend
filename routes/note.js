aded8f9a585e6f45395b1f79c637350db0cc34d2
Saksham Agrawal <71589032+sakshamagrawal07@users.noreply.github.com>
2025-04-22 15:08:04 +0530

    Update note.js

--- routes/note.js
+++ routes/note.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const noteController = require('../controllers/noteController');
+const { validateNote } = require('../middleware/noteValidators');
+const { validationResult } = require('express-validator');
 
-// CRUD operations
-router.post('/', noteController.createNote);
-router.get('/:userId', noteController.getNotes);
-router.get('/:userId/:noteId', noteController.getNoteById);
-router.put('/:userId/:noteId', noteController.updateNote);
-router.delete('/:userId/:noteId', noteController.deleteNote);
-router.get('/:userId/label/:label', noteController.getNotesByLabel);
+// Middleware to handle validation result
+const handleValidation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  next();
+};
+
+// Routes
+router.post('/create', validateNote, handleValidation, noteController.createNote);
+router.get('/user/:userId', noteController.getNotes);
+router.get('/user/:userId/:noteId', noteController.getNoteById);
+router.put('/user/:userId/:noteId', validateNote, handleValidation, noteController.updateNote);
+router.delete('/user/:userId/:noteId', noteController.deleteNote);
+router.get('/user/:userId/label/:label', noteController.getNotesByLabel);
 
 module.exports = router;
