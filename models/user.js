622aecf67d4c1563b9678d8868016e317e72d5a7
Saksham Agrawal <71589032+sakshamagrawal07@users.noreply.github.com>
2025-04-22 14:17:12 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,40 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    trim: true,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/.+\@.+\..+/, 'Invalid email address']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password during login
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
